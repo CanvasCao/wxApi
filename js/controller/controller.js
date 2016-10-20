2e08f1815c2f855018fdd4ee5682fb342da175e5
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-10-20 20:10:22 +0800

    aaa

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -60,5 +60,46 @@
         });
     }
 
+    controller.getProductList = function (params, callback) {
+        $.ajax({
+            type: "get",
+            url: jimiHost + '/getProductList.php',
+//                url: 'package.json',
+            data: params,
+            dataType: "jsonp",
+            jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
+            jsonpCallback: "jsonpcallback",
+            async: false,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (err) {
+                alert('ERROR!');
+                alert(JSON.stringify(err));
+            }
+        });
+    }
+    controller.saveProduct = function (params, callback) {
+        params.uid = GM.uid;
+        $.ajax({
+            type: "get",
+            url: jimiHost + '/saveProduct.php',
+//                url: 'package.json',
+            data: params,
+            dataType: "jsonp",
+            jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
+            jsonpCallback: "jsonpcallback",
+            async: false,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (err) {
+                alert('ERROR!');
+                alert(JSON.stringify(err));
+            }
+        });
+    }
+
+
     w.controller = controller;
 })(window, document, $)
